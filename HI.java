4cfa13f6ba920f32f2ab2249474a3a3f54bc0d2c
LazyDaily1231 <64627695+LazyDaily1231@users.noreply.github.com>
2021-06-22 18:03:38 +0900

    Add files via upload

---
+++ HI.java
@@ -0,0 +1,14 @@
+package start;
+
+public class HI {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("Hi");
+		System.out.println("Hello");
+		System.out.println("안녕");
+
+	}
+
+}
---
+++ Hello.java
@@ -0,0 +1,17 @@
+package start;//package는 항상 위로 와야함 
+
+public class Hello {
+
+	public static void main(String[] args) {
+		// Main 함수에 처리내용을 넣어보자
+		
+		//syso, sysout + Ctrl + SpaceBar=프린트라인 단축키
+		System.out.println("나는");
+		System.out.println("자동 완성");
+		System.out.println("반갑습니다");
+		System.out.println("Welcome");
+		
+		
+	}
+	
+}
